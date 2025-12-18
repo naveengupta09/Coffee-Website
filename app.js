735fa7e3e8513aa076ddb351b85c7215a825255d
Naveen09 <naveenkumar54111@gmail.com>
2025-12-18 15:01:18 +0530

    Implement mobile menu functionality and add new sections to the Coffee Website

---
+++ app.js
@@ -0,0 +1,28 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("clisk", () => menuOpenButton.click
+());
+
+//Initialize Swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
